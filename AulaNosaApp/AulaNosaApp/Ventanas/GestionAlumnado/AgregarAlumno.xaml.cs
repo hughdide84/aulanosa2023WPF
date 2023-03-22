311b08dd281e82d5f8f1711cb5300bc483caf319
DanielNovoNovo <106760565+DanielNovoNovo@users.noreply.github.com>
2023-03-22 21:25:08 +0100

    Control errores AgregarAlumnos

--- AulaNosaApp/AulaNosaApp/Ventanas/GestionAlumnado/AgregarAlumno.xaml.cs
+++ AulaNosaApp/AulaNosaApp/Ventanas/GestionAlumnado/AgregarAlumno.xaml.cs
@@ -83,18 +83,16 @@ namespace AulaNosaApp.Ventanas.GestionAlumnado
                 return;
             }
             alumno.idEmpresa = Empresa;
-           /* EmpresaDTO empresa = EmpresaApi.filtrarCursoId(Curso.ToString());
+            EmpresaDTO empresa = EmpresaAPI.consultarEmpresaId(Empresa);
             if (empresa == null)
             {
-                MessageBox.Show("El curso indicado no existe. Por favor, seleccione un curso válido.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("La empresa indicada no existe. Por favor, seleccione una empresa válida.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
-            }*/
+            }
             alumno.idEstudios = Estudios;
             EstudioDTO estudio = new EstudioDTO();
             estudio.id = Estudios;
-
             estudio = EstudioApi.filtrarEstudioId(Estudios.ToString());
-
             if (estudio == null)
             {
                 MessageBox.Show("El estudio introducido no existe. Introduzca un idEstudios válido.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
